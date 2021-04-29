ca0bcc71fd22450a06d5a060599b785f3235f3d9
n-ogawa <fy59.kou-729@live.jp>
2021-04-29 20:17:03 +0900

    Create simple report parser

--- __tests__/report.test.ts
+++ __tests__/report.test.ts
@@ -5,3 +5,8 @@ test('success report has no issue', () => {
   const report = new Report(path.join(__dirname, 'inspection_reports', 'success.xml'))
   expect(report.issues.length).toBe(0)
 })
+
+test('failure report has issues', () => {
+  const report = new Report(path.join(__dirname, 'inspection_reports', 'failure.xml'))
+  expect(report.issues.length).toBe(29)
+})
--- src/report.ts
+++ src/report.ts
@@ -1,8 +1,34 @@
 import {Issue} from './type'
+import * as fs from 'fs'
+import * as core from '@actions/core'
+import * as htmlparser2 from 'htmlparser2'
 
 export class Report {
   issues: Issue[]
-  constructor(private reportPath: string) {
+
+  constructor(reportPath: string) {
     this.issues = []
+
+    let file: string
+    try {
+      file = fs.readFileSync(reportPath, {encoding: 'utf8'})
+    } catch (err) {
+      core.error(err.message)
+      return
+    }
+
+    const xml = htmlparser2.parseDocument(file)
+    const issueTags = htmlparser2.DomUtils.getElementsByTagName('issue', xml)
+    for (const issue of issueTags) {
+      const typeId = issue.attributes.find(
+        a => a.name.toLowerCase() === 'typeid'
+      )
+      if (!typeId) {
+        continue
+      }
+      this.issues.push({
+        TypeId: typeId.value
+      })
+    }
   }
 }
