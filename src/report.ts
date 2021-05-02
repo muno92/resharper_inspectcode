4104be4d82b6ee04a1bf5738fcb594ee78c8c8f2
n-ogawa <fy59.kou-729@live.jp>
2021-05-02 23:11:02 +0900

    Extract parse issue method

--- src/report.ts
+++ src/report.ts
@@ -2,6 +2,7 @@ import {Issue} from './type'
 import * as fs from 'fs'
 import * as core from '@actions/core'
 import * as htmlparser2 from 'htmlparser2'
+import {Element} from 'domhandler'
 
 export class Report {
   issues: Issue[]
@@ -20,40 +21,48 @@ export class Report {
     const xml = htmlparser2.parseDocument(file)
     const issueTags = htmlparser2.DomUtils.getElementsByTagName('issue', xml)
     for (const issueTag of issueTags) {
-      const typeId = issueTag.attributes.find(
-        a => a.name.toLowerCase() === 'typeid'
-      )
-      const filePath = issueTag.attributes.find(
-        a => a.name.toLowerCase() === 'file'
-      )
-      const message = issueTag.attributes.find(
-        a => a.name.toLowerCase() === 'message'
-      )
+      const issue = Report.parseIssue(issueTag)
 
-      if (!typeId || !filePath || !message) {
+      if (!issue) {
         continue
-      }
-
-      const offset =
-        issueTag.attributes.find(a => a.name.toLowerCase() === 'offset')
-          ?.value ?? '0-0'
-      const column = parseInt(offset.substring(0, offset.indexOf('-')))
-
-      const issue: Issue = {
-        TypeId: typeId.value,
-        FilePath: filePath.value,
-        Column: column,
-        Message: message.value
-      }
-
-      const line = issueTag.attributes.find(
-        a => a.name.toLowerCase() === 'line'
-      )
-      if (line) {
-        issue.Line = parseInt(line.value)
       }
 
       this.issues.push(issue)
     }
+  }
+
+  private static parseIssue(issueTag: Element): Issue | null {
+    const typeId = issueTag.attributes.find(
+      a => a.name.toLowerCase() === 'typeid'
+    )
+    const filePath = issueTag.attributes.find(
+      a => a.name.toLowerCase() === 'file'
+    )
+    const message = issueTag.attributes.find(
+      a => a.name.toLowerCase() === 'message'
+    )
+
+    if (!typeId || !filePath || !message) {
+      return null
+    }
+
+    const offset =
+      issueTag.attributes.find(a => a.name.toLowerCase() === 'offset')?.value ??
+      '0-0'
+    const column = parseInt(offset.substring(0, offset.indexOf('-')))
+
+    const issue: Issue = {
+      TypeId: typeId.value,
+      FilePath: filePath.value,
+      Column: column,
+      Message: message.value
+    }
+
+    const line = issueTag.attributes.find(a => a.name.toLowerCase() === 'line')
+    if (line) {
+      issue.Line = parseInt(line.value)
+    }
+
+    return issue
   }
 }
