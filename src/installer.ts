662479c16d6350e3f11fc2ae68bcc3c1cab188ad
rony-legit <71696641+ron-legit@users.noreply.github.com>
2021-06-21 17:06:29 +0300

    Update installer.ts

--- src/installer.ts
+++ src/installer.ts
@@ -3,6 +3,6 @@ import * as exec from '@actions/exec'
 export class Installer {
   //TODO check dotnet sdk in constructor
   async install(): Promise<number> {
-    return exec.exec('dotnet tool install -g JetBrains.ReSharper.GlobalTools')
+    return exec.exec('dotnet tool install -g JetBrains.ReSharper.GlobalTools --version 2021.1.3')
   }
 }
