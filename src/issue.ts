b7beec70ea5275c3d628717396beee9d5f6a2b9f
n-ogawa <fy59.kou-729@live.jp>
2022-02-01 23:58:22 +0900

    Add Issue TypeId to output message (for #249)

--- src/issue.ts
+++ src/issue.ts
@@ -9,7 +9,7 @@ export class Issue {
   ) {}
 
   output(): string {
-    return `"${this.Message}" on ${this.FilePath}${
+    return `"[${this.TypeId}] ${this.Message}" on ${this.FilePath}${
       this.Line ? `(${this.Line},${this.Column})` : ''
     }`
   }
