119a449a9e6de0fa6ae47d866d2dde4ea46cd1fc
shay-legit <shay@legitsecurity.com>
2022-03-15 22:56:08 +0200

    fix conflicts

--- __tests__/issue.test.ts
+++ __tests__/issue.test.ts
@@ -10,13 +10,7 @@ test('output message with line and column number', () => {
     4
   )
 
-<<<<<<< HEAD
-  expect(issue.output()).toBe(
-    '"Using directive is not required by the code and can be safely removed" on /home/runner/work/ReShaperInspectionInCI/ReShaperInspectionInCI/ReShaperInspectionInCI/Controllers/WeatherForecastController.cs(4,67)'
-  )
-=======
   expect(issue.output()).toBe('"[RedundantUsingDirective] Using directive is not required by the code and can be safely removed" on /home/runner/work/ReShaperInspectionInCI/ReShaperInspectionInCI/ReShaperInspectionInCI/Controllers/WeatherForecastController.cs(4,67)')
->>>>>>> 537ae9d37a0dc8943e57058308b5a75bbb873101
 })
 
 test('output message without line and column number', () => {
@@ -28,11 +22,5 @@ test('output message without line and column number', () => {
     'warning'
   )
 
-<<<<<<< HEAD
-  expect(issue.output()).toBe(
-    '"Using directive is not required by the code and can be safely removed" on /home/runner/work/ReShaperInspectionInCI/ReShaperInspectionInCI/ReShaperInspectionInCI/Program.cs'
-  )
-=======
   expect(issue.output()).toBe('"[RedundantUsingDirective] Using directive is not required by the code and can be safely removed" on /home/runner/work/ReShaperInspectionInCI/ReShaperInspectionInCI/ReShaperInspectionInCI/Program.cs')
->>>>>>> 537ae9d37a0dc8943e57058308b5a75bbb873101
 })
