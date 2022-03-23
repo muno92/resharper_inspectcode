a1a74c5f79ee99d6df45fec24cd2003faa4750b9
shay-legit <shay@legitsecurity.com>
2022-03-24 00:07:07 +0200

    verbosity change

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function run(): Promise<void> {
     const solutionPath: string = path.join(cwd, core.getInput('solutionPath'))
     const outputPath = path.join(cwd, 'result.xml')
 
-    let command = `/root/.dotnet/tools/jb inspectcode -o=${outputPath} -a ${solutionPath} --build`
+    let command = `/root/.dotnet/tools/jb inspectcode -o=${outputPath} -a ${solutionPath} --build --verbosity=WARN`
 
     const exclude = core.getInput('exclude') ?? ''
     if (exclude !== '') {
