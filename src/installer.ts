397dbd92acec86e1fb20e300a9ac8ae371b618bc
shay-legit <shay@legitsecurity.com>
2022-03-27 09:10:00 +0300

    Update installer.ts

--- src/installer.ts
+++ src/installer.ts
@@ -4,10 +4,10 @@ import * as io from '@actions/io'
 
 export class Installer {
   //TODO check dotnet sdk in constructor
-  async install(version: string): Promise<number> {
+  async install(version: string, executablePath: string): Promise<number> {
     // If JetBrains.ReSharper.GlobalTools is already installed, skip installation to avoid install error.
     try {
-      await io.which('/github/home/.dotnet/tools/jb', true)
+      await io.which(`${executablePath}`, true)
       core.info(
         'JetBrains.ReSharper.GlobalTools is already installed, so skip installation.'
       )
