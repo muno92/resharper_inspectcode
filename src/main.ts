f574ebb10eb4e64c0e75c690c4dfec123a2f7d7e
nirb-legit <93517615+nirbashan@users.noreply.github.com>
2022-04-28 15:03:57 +0300

    allow remove tests flag

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,25 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import {Installer} from './installer'
 import {Report} from './report'
+import {fs} from 'fs';
 import path from 'path'
+
 
 async function run(): Promise<void> {
   try {
     const installer = new Installer()
     const version: string = core.getInput('version')
     const executablePath = core.getInput('executablePath') ?? 'jb'
+    const removeTests = core.getInput('shouldRemoveTests') ?? false
     await installer.install(version, executablePath)
 
     const cwd = process.cwd()
 
     const solutionPath: string = path.join(cwd, core.getInput('solutionPath'))
+    if (removeTests) {
+      fs.rmdirSync(`${solutionPath}\\tests`, { recursive: true })
+    }
+    
     const outputPath = path.join(cwd, 'result.xml')
 
     let command = `${executablePath} inspectcode -o=${outputPath} -a ${solutionPath} --build --verbosity=WARN`
