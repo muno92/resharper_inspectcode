1eca1dd88beedad082085db90b92f56b8147e046
n-ogawa <fy59.kou-729@live.jp>
2022-05-05 13:07:22 +0900

    Add comment

--- src/installer.ts
+++ src/installer.ts
@@ -3,6 +3,7 @@ import * as exec from '@actions/exec'
 export class Installer {
   //TODO check dotnet sdk in constructor
   async install(version: string): Promise<number> {
+    // The 'update' command will install JetBrains.ReSharper.GlobalTools if it is not already installed.
     let command = `dotnet tool update --global JetBrains.ReSharper.GlobalTools`
 
     if (version !== '') {
